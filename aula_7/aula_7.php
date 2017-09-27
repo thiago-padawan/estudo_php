b2b9153a6500bbf29e339644d09b0d62e10c2103
Thiago Henrique <sor_thiago@hotmail.com>
2017-09-27 15:40:39 -0300

    Rotinas pt1 e pt2

---
+++ aula_7/aula_7.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 7 -Chamando funções externas</title>
+  </head>
+  <body>
+    <?php
+        require "funcoes_externas.php";
+        echo "<h1>Testando FUNÇÕES EXTERNAS</h1>";
+        mostra_valor(ola());
+     ?>
+  </body>
+</html>
---
+++ aula_7/funcoes_externas.php
@@ -0,0 +1,8 @@
+<?php
+  function ola(){
+    return "<h1>Olarrr</h1>";
+  }
+  function mostra_valor($v){
+    echo "Recebi o valor : ".$v;
+  }
+ ?>
